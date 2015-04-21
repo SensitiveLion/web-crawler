b3b0b46856cd38706461e3f39aff48a0f5d05109
SensitiveLion <andypassidomo@gmail.com>
2015-04-21 16:07:44 -0400

    github update

--- app/controllers/homes_controller.rb
+++ app/controllers/homes_controller.rb
@@ -2,7 +2,7 @@ class HomesController < ApplicationController
   def index
     @goggle = Mechanize.new
     @page = @goggle.get('http://www.wow.com/')
-    @goggle_form = @page.form('q')
+    @goggle_form = @page.form('SearchBoxForm')
     @goggle_form.q = 'ruby mechanize'
     @page = @goggle.submit(@goggle_form)
   end
--- crawl.rb
+++ crawl.rb
@@ -4,9 +4,10 @@ require "pry"
 
 agent = Mechanize.new
 page = agent.get('http://bing.com')
+pp page
 bing_form = page.form()
 bing_form.q = 'ruby mechanize'
 page = agent.submit(bing_form)
 page.links.each do |link|
-  puts link.href.gsub(/^(?!http(.*)).*/,"")
+  puts link.href
 end
